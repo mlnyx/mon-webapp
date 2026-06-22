3883e41dd22d6ea4e37e68c71faccc8d3ce51b44
mlnyx <mlnyx104@gmail.com>
2026-06-22 21:47:31 +0900

    웹앱 2페이지 재편(실행/통계) + 통장 자금계획

---
+++ _next/static/RPx3hlk3LSsW8wH68H5sv/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/mon-webapp//_next/:path+",
+        "destination": "/mon-webapp/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/0nqabjpvim86k.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,26999,e=>{"use strict";var r=e.i(9107);e.s(["default",0,function(){return(0,r.useEffect)(()=>{"serviceWorker"in navigator&&navigator.serviceWorker.register("/mon-webapp/sw.js",{scope:"/mon-webapp/"}).catch(()=>{})},[]),null}])}]);
